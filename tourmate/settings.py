92c3ee5e05eca53e8d7b2684a63ae68c2888460f
Ronak Bhagchandani <37010825+ronakkkk@users.noreply.github.com>
2023-04-26 00:46:58 +0530

    Update settings.py

--- tourmate/settings.py
+++ tourmate/settings.py
@@ -163,6 +163,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
